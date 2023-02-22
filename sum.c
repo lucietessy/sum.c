444cd09442adfbdc5ea70378b7677aab6d1576e7
lucietessy <123073199+lucietessy@users.noreply.github.com>
2023-02-22 16:03:27 +0300

    Add files via upload

---
+++ sum.c
@@ -0,0 +1,13 @@
+//program to find sum of first 10 natural numbers 
+#include <stdio.h>
+int main()
+{
+    int x, sum;
+    printf("\n enter the first 10 natural numbers:");
+    scanf("%d",&x)
+    printf("sum of first 10 natural numbers")
+    if ("x=1,x<=10, x++")
+    sum=sum+x;
+    return 0;
+}
+  
